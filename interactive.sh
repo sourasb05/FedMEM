33b429aaa7e52c6bd63b5f9f023f921f2439668c
sourasb05 <sourasb05@gmail.com>
2024-04-03 02:53:04 +0200

    confusion_matrix_results_added

--- interactive.sh
+++ interactive.sh
@@ -1,1 +1,7 @@
-interactive --gpus=1 -t 01:00:00
+interactive --gpus=1 -t 00:45:00 --account=berzelius-2024-110
+interactive --gpus=1 -t 01:00:00 --account=berzelius-2024-110
+interactive --gpus=1 -t 01:30:00 --account=berzelius-2024-110
+interactive --gpus=1 -t 02:00:00 --account=berzelius-2024-110
+
+
+conda activate personalized_fl
