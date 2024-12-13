634a7f77a877bd9454094846eafdc50a451a2425
sourasb05 <40869271+sourasb05@users.noreply.github.com>
2024-12-13 16:06:58 +0100

    Delete interactive.sh

--- interactive.sh
+++
@@ -1,7 +0,0 @@
-interactive --gpus=1 -t 00:45:00 --account=berzelius-2024-110
-interactive --gpus=1 -t 01:00:00 --account=berzelius-2024-110
-interactive --gpus=1 -t 01:30:00 --account=berzelius-2024-110
-interactive --gpus=1 -t 02:00:00 --account=berzelius-2024-110
-
-
-conda activate personalized_fl
